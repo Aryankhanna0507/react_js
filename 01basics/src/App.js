d18ff123f53fb8a2688a93c4865d2810eb8dac38
Aryan <khannaaryan460@gmial.com>
2026-05-31 14:30:04 +0530

    creating the chai.js to understand more things

--- 01basics/src/App.js
+++ 01basics/src/App.js
@@ -1,7 +1,14 @@
-
+import Chai from './chai'
 function App() {
   return (
+    <>
+    <Chai/>
     <h1>chai or code</h1>
+    </>
+    // jo function aap imprt kr rahee ho wo ek component hi hai
+    // best practices 
+    // chahe vite ka use krke banao ya phir normal react ka use krke in component function ka name must be capitalize otherwise it will give us eror sath me file ka name bhi capitalize hi rakha kro 
+    // some library will force you ki component ka naam jsx ke rakho na ki keval js se ex-vite
   );
 }
 
